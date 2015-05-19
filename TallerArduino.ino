d96f7a080409c1ca01f07bef30376407f2d478fa
José Antonio Yébenes <joseayebenes@gmail.com>
2015-05-19 20:26:14 +0200

    Sesión 3 FINAL

--- TallerArduino.ino
+++ TallerArduino.ino
@@ -50,7 +50,7 @@ void setup() {
   pinMode(IN3Pin, OUTPUT);
   pinMode(IN4Pin, OUTPUT);
   pinMode(ENBPin, OUTPUT);
-  
+  //INICIALIZAMOS LOS MOTORES
   digitalWrite(ENAPin,LOW);
   digitalWrite(ENBPin,LOW);
   digitalWrite(IN1Pin,HIGH);
@@ -78,39 +78,23 @@ void loop() {
 void procesarComando(){
   switch(comando){
     case 'w':
-      digitalWrite(ENAPin,HIGH);
-      digitalWrite(ENBPin,HIGH);
-      delay(20);
-      analogWrite(ENAPin,velD);
-      analogWrite(ENBPin,velI);
-      avanzando=true;
+      // AVANZAR
     break;
     case 's':
-      digitalWrite(ENAPin,LOW);
-      digitalWrite(ENBPin,LOW);
-      avanzando=false;
+      // PARAR
     break;
     case 'a':
-      avanzando=false;
-      digitalWrite(ENBPin,LOW);
-      digitalWrite(ENAPin,HIGH);
-      delay(7);
-      analogWrite(ENAPin,velD);
-      
+      // GIRAR IZQUIERDA      
     break;
     case 'd':
-      avanzando=false;
-      digitalWrite(ENAPin,LOW);
-      digitalWrite(ENBPin,HIGH);
-      delay(7);
-      analogWrite(ENBPin,velI);
+      // GIRAR DERECHA
     break;
     case 'l':
-      Serial.println(leerDistancia());
-    break;
+      // LEER y MOSTRAR DISTANCIA
+      break;
     case 'p':
-      displayInfo();
-    break;   
+      // LEER y MOSTRAR POSICION GPS
+      break;   
     default:
        Serial.println("Comando Desconocido");
   }
@@ -119,68 +103,13 @@ void procesarComando(){
 
 void displayInfo()
 {
-  Serial.print(F("Location: ")); 
-  if (gps.location.isValid())
-  {
-    Serial.print(gps.location.lat(), 6);
-    Serial.print(F(","));
-    Serial.print(gps.location.lng(), 6);
-  }
-  else
-  {
-    Serial.print(F("INVALID"));
-  }
-  Serial.print(F("  Date/Time: "));
-  if (gps.date.isValid())
-  {
-    Serial.print(gps.date.month());
-    Serial.print(F("/"));
-    Serial.print(gps.date.day());
-    Serial.print(F("/"));
-    Serial.print(gps.date.year());
-  }
-  else
-  {
-    Serial.print(F("INVALID"));
-  }
-  Serial.print(F(" "));
-  if (gps.time.isValid())
-  {
-    if (gps.time.hour() < 10) Serial.print(F("0"));
-    Serial.print(gps.time.hour());
-    Serial.print(F(":"));
-    if (gps.time.minute() < 10) Serial.print(F("0"));
-    Serial.print(gps.time.minute());
-    Serial.print(F(":"));
-    if (gps.time.second() < 10) Serial.print(F("0"));
-    Serial.print(gps.time.second());
-    Serial.print(F("."));
-    if (gps.time.centisecond() < 10) Serial.print(F("0"));
-    Serial.print(gps.time.centisecond());
-  }
-  else
-  {
-    Serial.print(F("INVALID"));
-  }
-
-  Serial.println();
+  // LLAMAR A FUNCIONES DE LA LIBRERIA Y MOSTRAR POR PANTALLA
+  // LATITUD, LONGITUD, ALTITUD, HORA, FECHA, VELOCIDAD
 }
-
 float leerDistancia(){
-  float tiempo;
-  float distancia;
-  digitalWrite(trigPin,LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH); 
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
+  float distancia=0;
   
-  tiempo=pulseIn(echoPin, HIGH, 8000);
-  
-  if(tiempo ==8000){
-    return 555;
-  }
-  distancia= tiempo*0.017;
+  //CALCULAR DISTANCIA
   
   return distancia;
 }
@@ -188,31 +117,29 @@ float leerDistancia(){
 
 void controlarAvance(){
   
-  valI=analogRead(0);
+  valI=analogRead(EIPin);
   if(valI<sens)
     statI=LOW;
    else
     statI=HIGH;
 
-   if(stat2I!=statI){  //counts when the state change, thats from (dark to light) or 
-                     //from (light to dark), remmember that IR light is invisible for us.
+   if(stat2I!=statI){  
      contarI++;
      stat2I=statI;
    }
    
-   valD=analogRead(1);
+   valD=analogRead(EDPin);
   if(valD<sens)
     statD=LOW;
    else
     statD=HIGH;
 
-   if(stat2D!=statD){  //counts when the state change, thats from (dark to light) or 
-                     //from (light to dark), remmember that IR light is invisible for us.
+   if(stat2D!=statD){  
      contarD++;
      stat2D=statD;
    }
      
-    if(millis()-last>=1){
+    if(millis()-last>=20){
       if(contarD>contarI){
         if(velD>0){velD--;}
       }else{
